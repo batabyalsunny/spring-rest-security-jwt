1f978f6bf83384da3fc95ec0a8b0449fbb761f7f
Sunny Batabyal <batabyalsunny@gmail.com>
2019-07-08 09:39:48 +0530

    Initial project setup

---
+++ src/main/java/ml/bootcode/springrestsecurityjwt/SpringRestSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package ml.bootcode.springrestsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ml/bootcode/springrestsecurityjwt/SpringRestSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.bootcode.springrestsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
